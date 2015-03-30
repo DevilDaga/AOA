d62846c772b31dd9804d81e423da5eea632e5423
DevilDaga <dagaddevil@gmail.com>
2015-03-31 01:12:06 +0530

    Untested.

--- 15 Puzzle/Source.c
+++ 15 Puzzle/Source.c
@@ -5,15 +5,16 @@
 typedef enum
 {
 	RESERVE	=	0,
-	UP,
-	DOWN,
-	LEFT,
-	RIGHT
+	UP		=	1,
+	DOWN	=	2,
+	LEFT	=	3,
+	RIGHT	=	4
 }DIRECTION;
 
-typedef struct
+typedef struct node_s
 {
-	int **grid;
+	struct node_s *parent;
+	int **grid, x, y;
 	int cost;
 }node;
 
@@ -21,16 +22,16 @@ typedef struct
 #define LCHILD(x)		(2 * (x))
 #define RCHILD(x)		(2 * (x) + 1)
 
-void min_heapify ( node *heap, size_t i, size_t n )
+void min_heapify ( node **heap, size_t i, size_t n )
 {
 	size_t l = LCHILD ( i ),
 		r = RCHILD ( i ),
 		largest;
-	if ( l < n && heap[ l ].cost < heap[ i ].cost )
+	if ( l < n && heap[ l ]->cost < heap[ i ]->cost )
 		largest = l;
 	else
 		largest = i;
-	if ( r < n && heap[ r ].cost < heap[ largest ].cost )
+	if ( r < n && heap[ r ]->cost < heap[ largest ]->cost )
 		largest = r;
 	if ( largest != i )
 	{
@@ -39,19 +40,19 @@ void min_heapify ( node *heap, size_t i, size_t n )
 	}
 }
 
-void insert ( node *heap, size_t n, node ele )
+void insert ( node **heap, size_t n, node *ele )
 {
 	heap[ n ] = ele;
-	while ( heap[ n ].cost > heap[ PARENT ( n ) ].cost )
+	while ( heap[ n ]->cost > heap[ PARENT ( n ) ]->cost )
 	{
 		swap ( heap + n, heap + PARENT ( n ), sizeof *heap );
 		n = PARENT ( n );
 	}
 }
 
-node extract ( node *heap, size_t n )
+node *extract ( node **heap, size_t n )
 {
-	node result = heap[ 0 ];
+	node *result = heap[ 0 ];
 	heap[ 0 ] = heap[ n - 1 ];
 	min_heapify ( heap, 0, n - 1 );
 	return result;
@@ -77,7 +78,7 @@ int manhattan ( int **grid, int x_blank, int y_blank )
 	return result;
 }
 
-int **move ( int **grid, int x_blank, int y_blank, DIRECTION dir )
+int **move ( int **grid, int *x_blank, int *y_blank, DIRECTION dir )
 {
 	int **result, x, y, i;
 	M_ALLOCATE ( result, SIZE );
@@ -89,36 +90,64 @@ int **move ( int **grid, int x_blank, int y_blank, DIRECTION dir )
 	switch ( dir )
 	{
 		case UP:
-			x = x_blank;
-			y = ( y_blank + SIZE - 1 ) % SIZE;
+			x = *x_blank;
+			y = ( *y_blank + SIZE - 1 ) % SIZE;
 			break;
 		case DOWN:
-			x = x_blank;
-			y = ( y_blank + SIZE + 1 ) % SIZE;
+			x = *x_blank;
+			y = ( *y_blank + SIZE + 1 ) % SIZE;
 			break;
 		case LEFT:
-			x = ( x_blank + SIZE - 1 ) % SIZE;
-			y = y_blank;
+			x = ( *x_blank + SIZE - 1 ) % SIZE;
+			y = *y_blank;
 			break;
 		case RIGHT:
-			x = ( x_blank + SIZE + 1 ) % SIZE;
-			y = y_blank;
+			x = ( *x_blank + SIZE + 1 ) % SIZE;
+			y = *y_blank;
 			break;
 	}
-	swap ( &result[ x ][ y ], &result[ x_blank ][ y_blank ],
+	swap ( &result[ x ][ y ], &result[ *x_blank ][ *y_blank ],
 		   sizeof **grid );
+	*x_blank = x;
+	*y_blank = y;
 	return result;
 }
 
-DIRECTION *solve ( int **grid, int x_blank, int y_blank )
+node *solve ( int **grid, int x_blank, int y_blank )
 {
 	size_t k = 0, top = 0;
-	DIRECTION *result;
-	int ***stack;
-	M_ALLOCATE ( stack, 1024 );
-	C_ALLOCATE ( result, 256 );
-	stack[ top++ ] = grid;
-	return result;
+	node **heap, *T;
+	M_ALLOCATE ( heap, 1024 );
+	C_ALLOCATE ( T, 1 );
+	T->cost = manhattan ( grid, x_blank, y_blank );
+	T->grid == grid;
+	T->x = x_blank;
+	T->y = y_blank;
+	if ( T->cost == 0 )
+		return T;
+	insert ( heap, k++, T );
+	while ( 1 )
+	{
+		int i;
+		node *E = extract ( heap, k-- );
+		for ( i = 1; i != 5; ++i )
+		{
+			node *X;
+			C_ALLOCATE ( X, 1 );
+			int x = E->x, y = E->y;
+			int **child = move ( E->grid, &x, &y, i ),
+				cost = manhattan ( child, x, y );
+			X->grid = child;
+			X->cost = cost;
+			X->parent = E;
+			X->x = x;
+			X->y = y;
+			if ( cost == 0 )
+				return X;		// Solution found
+			insert ( heap, k++, X );
+		}
+	}
+	return NULL;
 }
 
 void main ( )
